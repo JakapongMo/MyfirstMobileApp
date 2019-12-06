457c91a794553737e71503536bedef871cdbe8b0
Jakapong <jakapong.klainongsuang@refinitiv.com>
2019-12-06 19:49:29 +0700

    Basic Dart Learning

---
+++ lib/learnDart.dart
@@ -0,0 +1,23 @@
+class Person {
+  String name;
+  int age;
+  
+  //Constructor
+  Person({String this.name, int this.age = 30});
+}
+
+double addNumbers(double num1, double num2) {
+//   print(num1 + num2);
+  return num1 + num2;
+}
+
+void main() {
+  var p1 = Person(name: 'Max');
+  var p2 = Person(age: 31, name: 'Manu');
+  print(p1.name);
+  print(p2.name);
+  double firstResult;
+  firstResult = addNumbers(1,1);
+  print(firstResult + 1);
+  print('Hello!');
+}
